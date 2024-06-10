51740201439e6a2fb518a72d61e985ef126fb13d
Bimal dev <bimaldev707@gmail.com>
2024-06-10 17:28:22 +0530

    completed

--- feedback_frontend/src/componenets/FeedbackFeature.jsx
+++ feedback_frontend/src/componenets/FeedbackFeature.jsx
@@ -45,6 +45,58 @@ const FeedbackFeature = () => {
 
 
           </div>
+          <div className='answer'>
+            <p className='ans-text'>Answers(23)</p>
+            <div className='sort-popular'>
+            <p className='sort'>Sort By:</p>
+            <div class="dropdown">
+              <button class="dropbtn">Popular &#9662;</button>
+              <div class="dropdown-content">
+               </div>
+              </div>
+
+            </div>
+          
+
+          </div>
+
+          <div className='container-2'>
+            <div className='first-div' >
+              <div className='pic-name'>
+              <img className='neha-pic' src='/images/Ellipse 52.png' />
+              <div className='name'>
+              <p className='neha-name'><strong>Neha Bhat</strong> (You)</p>
+              <p className='date'>Jun 27, 2023</p>
+              </div>
+              </div>
+              <div className='edit-with-pic' >
+                <img className='edit-img' src='/images/edit.png' />
+                <p className='edit-word' >Edit</p>
+
+              </div>
+            </div>
+            <div className='Text-c2'>
+             <label className='a'>Lorem ipsum dolor sit amet consectetur. Elit et ut at vestibulum enim ornare feugi vitae. Eget proin aliquam blandit eget vitae erat fermentum lacus. Dignissim done mi vel fermentum. Id ultrices risus sit pel sit elit morbi. Mi sed mauris aenean odio egestas ullamcorper. Dignissim in vel fusce id. Sit blandit diam ridiculus ipsum </label>
+              <label className='b'>interdum ut velit quam. Bibendum amet mi....</label>
+               <label className='c'>Show more</label>
+
+            </div>
+            <div className='last-div'>
+              <img className='like-img'src='/images/like.png' />
+              <p className='like-word'>Like</p>
+              <img className='msg-img' src='/images/msg.png'/>
+              <input 
+                className='text-field' 
+                type='text' 
+                // value={answer} 
+                // onChange={handleInputChange} 
+                placeholder='Add a comment'
+              />
+              <button className='post'>Post</button>
+
+            </div>
+
+          </div>
            
 
         </div>
